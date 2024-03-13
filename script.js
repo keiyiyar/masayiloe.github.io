c041dd208ec678584021c3473d8b8600037a58b4
keiyiyar <163116750+keiyiyar@users.noreply.github.com>
2024-03-13 10:21:57 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+  const jumpButton = document.getElementById('jumpButton');
+
+  jumpButton.addEventListener('click', function() {
+    // 这里你可以添加跳转逻辑，比如跳转到另一个页面或者滚动到页面的某个位置
+    alert('https://m.masayaph.com/');
+  });
+});
