2765ff38dfde24799329446e613ba7cac7c9eb54
keiyiyar <163116750+keiyiyar@users.noreply.github.com>
2024-03-13 10:36:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
   const jumpButton = document.getElementById('jumpButton');
 
   jumpButton.addEventListener('click', function() {
-    window.location.href = 'https://m.masayaph.com/';
+    // 点击按钮时弹出确认框
+    const ready = confirm('准备好了吗？');
+    
+    // 如果点击了确认，则跳转到另一个网站
+    if (ready) {
+      window.location.href = 'https://m.masayaph.com';
+    }
   });
 });
